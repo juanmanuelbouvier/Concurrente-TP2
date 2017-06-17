626fe64d59a30a6e1667620930ed3b1b2ee078cb
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-06-17 14:07:30 -0300

    Agrego struct Persona al gestor

---
+++ Gestor-BdD/include/Persona.h
@@ -0,0 +1,16 @@
+
+#ifndef GESTOR_BDD_PERSONA_H
+#define GESTOR_BDD_PERSONA_H
+
+#define MAX_NOMBRE      61
+#define MAX_DIRECCION   120
+#define MAX_TELEFONO    13
+
+typedef struct Persona {
+    long mtype;
+    char* nombre [MAX_NOMBRE];
+    char* direccion [MAX_DIRECCION];
+    char* telefono [MAX_TELEFONO];
+} Persona;
+
+#endif //GESTOR_BDD_PERSONA_H
