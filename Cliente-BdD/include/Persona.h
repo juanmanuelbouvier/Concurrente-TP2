27a5cb81bc4f6c272ee85413afd1938072892dd2
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-06-17 14:04:14 -0300

    Struct de Persona agregado en cliente

---
+++ Cliente-BdD/include/Persona.h
@@ -0,0 +1,17 @@
+
+#ifndef CLIENTE_BDD_PERSONA_H
+#define CLIENTE_BDD_PERSONA_H
+
+#define MAX_NOMBRE      61
+#define MAX_DIRECCION   120
+#define MAX_TELEFONO    13
+
+typedef struct Persona {
+    long mtype;
+    char* nombre [MAX_NOMBRE];
+    char* direccion [MAX_DIRECCION];
+    char* telefono [MAX_TELEFONO];
+} Persona;
+
+
+#endif //CLIENTE_BDD_PERSONA_H
