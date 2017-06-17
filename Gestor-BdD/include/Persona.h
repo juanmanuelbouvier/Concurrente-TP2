0d20e6998edb00c4806a1bdbb4437351d49af9ec
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-06-17 17:48:45 -0300

    cambio boolean en persona por bool (nativo)

--- Gestor-BdD/include/Persona.h
+++ Gestor-BdD/include/Persona.h
@@ -2,8 +2,6 @@
 #ifndef GESTOR_BDD_PERSONA_H
 #define GESTOR_BDD_PERSONA_H
 
-#include <jmorecfg.h>
-
 #define MAX_NOMBRE      61
 #define MAX_DIRECCION   120
 #define MAX_TELEFONO    13
@@ -13,7 +11,7 @@ typedef struct Persona {
     char nombre [MAX_NOMBRE];
     char direccion [MAX_DIRECCION];
     char telefono [MAX_TELEFONO];
-    boolean esUnicoResultado;
+    bool esUnicoResultado;
 } Persona;
 
 #endif //GESTOR_BDD_PERSONA_H
