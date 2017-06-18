dd50d22a0dbb3b24ef857ff33790d9fc86b7177c
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-06-18 15:53:44 -0300

    El main del gestor va a crear el archivo temporal con cantidad de clientes y borrarlo al final de todo.

--- Gestor-BdD/src/Main.cpp
+++ Gestor-BdD/src/Main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include "../include/DespachanteConsultas.h"
 
+int crearDirectorioSiNoExiste(string rutaCompletaArchivo) {
+    string path = rutaCompletaArchivo.substr(0, rutaCompletaArchivo.find_last_of("\\/"));
+    DIR* dir = opendir(path.c_str());
+    if (dir) {
+        return closedir(dir);
+    }
+    else if (ENOENT == errno) {
+        return mkdir(path.c_str(), 0777);
+    }
+    return -1;
+}
+
+int crearArchivoTemporal(const string archivoTmp) {
+    crearDirectorioSiNoExiste(archivoTmp);
+    int fdTmp = open ( archivoTmp.c_str(), O_CREAT|O_EXCL|O_RDONLY, 0777 );
+    return close( fdTmp );
+}
+
 int main() {
     std::cout << "Soy gestor!" << std::endl;
 
-    // TODO: Crear archivo temporal de cantClientes.txt que usan los clientes e inicializarlo en 0.
+    const string archivoTmp = "../../tmp/cantClientes.txt";
+    crearArchivoTemporal(archivoTmp);
 
     DespachanteConsultas despachante = DespachanteConsultas ();
     while (true) {  // TODO: Usar gracefulQuit con signals para matar el servidor.
@@ -12,7 +31,7 @@ int main() {
         sleep(1);   // TODO: Si lo sacamos se me muere la compu.
     }
 
-    // TODO: Borrar archivo temporal cantClientes.txt
+    unlink (archivoTmp.c_str());
 
     return 0;
 }
