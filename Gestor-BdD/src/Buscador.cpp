8e61eae9811ba6b018d8a61679c58e717250fe1f
guidonegri <bokita_93@hotmail.com>
2017-06-20 17:50:10 -0300

    Soy,octa,no,se,separar,las,comas,salu2

--- Gestor-BdD/src/Buscador.cpp
+++ Gestor-BdD/src/Buscador.cpp
@@ -17,24 +17,30 @@ vector<Persona> Buscador::buscar(Persona filtros) {
     vector<Persona> resultado = vector<Persona>();
     std::ifstream input(RUTA_ARCHIVO_BDD);
     std::string linea, nombreBuscado = "$", direccionBuscada = "$", telefonoBuscado = "$";
+
     if (nombreBuscado.length() > 0) nombreBuscado = filtros.nombre ;
     if (direccionBuscada.length() > 0)direccionBuscada = filtros.direccion;
     if (telefonoBuscado.length() > 0) telefonoBuscado = filtros.telefono;
-    std::regex regNombre(nombreBuscado,regex_constants::icase),regDireccion(direccionBuscada,regex_constants::icase),
+    std::regex regNombre(nombreBuscado, regex_constants::icase),regDireccion(direccionBuscada, regex_constants::icase),
             regTelefono(telefonoBuscado);
-    while (std::getline(input,linea)) {
-        std::string nombreLinea,direccionLinea,telefonoLinea;
-        nombreLinea = linea.substr(0,linea.find(DELIMITADOR));
-        linea =  linea.substr(linea.find(DELIMITADOR)+1,linea.length());
-        direccionLinea =  linea.substr(0,linea.find(DELIMITADOR));
-        linea = linea.substr(linea.find(DELIMITADOR)+1,linea.length());
+
+    while (std::getline(input, linea)) {
+        std::string nombreLinea, direccionLinea, telefonoLinea;
+
+        nombreLinea = linea.substr(0, linea.find(DELIMITADOR));
+
+        linea =  linea.substr(linea.find(DELIMITADOR)+1, linea.length());
+        direccionLinea =  linea.substr(0, linea.find(DELIMITADOR));
+
+        linea = linea.substr(linea.find(DELIMITADOR)+1, linea.length());
         telefonoLinea = linea;
-        if (regex_search(nombreLinea,regNombre) && regex_search(direccionLinea,regDireccion) &&
-                regex_search(telefonoLinea,regTelefono)) {
+
+        if (regex_search(nombreLinea, regNombre) && regex_search(direccionLinea, regDireccion) &&
+                regex_search(telefonoLinea, regTelefono)) {
             Persona personaValida;
-            strcpy(personaValida.nombre,nombreLinea.c_str());
-            strcpy(personaValida.direccion,direccionLinea.c_str());
-            strcpy(personaValida.telefono,telefonoLinea.c_str());
+            strcpy(personaValida.nombre, nombreLinea.c_str());
+            strcpy(personaValida.direccion, direccionLinea.c_str());
+            strcpy(personaValida.telefono, telefonoLinea.c_str());
             personaValida.faltanResultados = true;
             resultado.push_back(personaValida);
         }
